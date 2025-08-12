58cc9b5c773877b2a1211ba3eda0cadbb3c89133
shamvamshi <shamvamshiwork@gmail.com>
2025-08-12 13:00:48 +0530

    test

--- locals.tf
+++ locals.tf
@@ -1,14 +1,3 @@
-
-
-# locals {
-#   common_tags = {
-#     Name = var.project
-#     environment = var.environment
-#     terraform = "true"
-#   }
-# }
-
-
 locals {
   common_tags = {
     project = var.project
